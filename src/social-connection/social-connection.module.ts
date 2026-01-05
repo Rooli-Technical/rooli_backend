8103b436ec6ca15c2f882055227a818abea02f58
Nanret123 <nanretgungshik@gmail.com>
2026-01-05 21:41:55 +0100

    feat: added service to provider

--- src/social-connection/social-connection.module.ts
+++ src/social-connection/social-connection.module.ts
@@ -4,11 +4,12 @@ import { SocialConnectionController } from './social-connection.controller';
 import { EncryptionService } from '@/common/utility/encryption.service';
 import { FacebookService } from './providers/facebook.service';
 import { HttpModule } from '@nestjs/axios';
+import { LinkedInService } from './providers/linkedin.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [SocialConnectionController],
-  providers: [SocialConnectionService, EncryptionService, FacebookService],
+  providers: [SocialConnectionService, EncryptionService, FacebookService, LinkedInService],
   exports:[SocialConnectionService]
 })
 export class SocialConnectionModule {}
