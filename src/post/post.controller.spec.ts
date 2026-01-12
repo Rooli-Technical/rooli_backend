344b52ebf7ad290845b343c229610aa0c920ca0b
Nanret123 <nanretgungshik@gmail.com>
2026-01-12 14:09:59 +0100

    fix

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './controllers/post.controller';
-import { PostService } from './post.service';
+import { PostService } from './services/post.service';
 
 describe('PostController', () => {
   let controller: PostController;
