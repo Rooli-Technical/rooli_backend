986854a6e3bc6078193dfb305829fc8d3f6e7aa0
Nanret123 <nanretgungshik@gmail.com>
2026-02-18 18:36:26 +0100

    fix: add member controller and service to module

--- src/workspace/workspace.module.ts
+++ src/workspace/workspace.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { WorkspaceService } from './workspace.service';
 import { WorkspaceController } from './workspace.controller';
 import { AuthModule } from '@/auth/auth.module';
+import { WorkspaceMemberService } from './members/member.service';
+import { WorkspaceMemberController } from './members/controller/member.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [WorkspaceController],
-  providers: [WorkspaceService],
+  controllers: [WorkspaceController, WorkspaceMemberController],
+  providers: [WorkspaceService, WorkspaceMemberService],
 })
 export class WorkspaceModule {}
