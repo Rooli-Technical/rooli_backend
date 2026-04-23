817f84f83ec1311e46f43ba8a12dfa85c81196a3
Nanret123 <nanretgungshik@gmail.com>
2026-04-23 11:27:38 +0100

    fix

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -4,14 +4,14 @@ import { seedPlans } from './seed-plans';
 import { seedRBAC } from './seed-rbac';
 
 async function main() {
-  // console.log('Seeding rbac...');
-  //  await seedRBAC();
+  console.log('Seeding rbac...');
+   await seedRBAC();
 
   // console.log('Seeding users...');
   // await seedUsers();
 
-  console.log('Seeding plans...');
-  await seedPlans();
+  // console.log('Seeding plans...');
+  // await seedPlans();
 
   console.log('✔ All seeders completed');
 }
